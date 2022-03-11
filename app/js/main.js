8603836dfa25ef5cfff70c96b59611c87e7f822c
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2022-03-11 16:23:22 +0300

    added _testimonials

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,24 @@
 $(function () {
 
   $('.navbar__btn').on('click', function () {
+
     $(this).toggleClass('navbar__btn--active');
     $('.navbar-menu').toggleClass('navbar-menu--active');
+
   })
 
   $('.view-progects__item').hover(function () {
-    
+
     let popupIndex = $('.view-progects__popup')[$(this).index()];
+    $('.view-progects__popup').removeClass('view-progects__popup--active');
     $(popupIndex).toggleClass('view-progects__popup--active');
 
+  })
+
+  $('.testimonials__slider').slick({
+    infinite: false,
+    speed: 700,
+    // autoplay: true,
   })
 
 });
