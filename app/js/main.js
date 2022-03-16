0de2ed23653588cfca9a3069fccd3bbae10f5ddd
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2022-03-16 14:58:23 +0300

    finished FAQ, aboutUs, blog, blog-read, contactUs, work-details

--- app/js/main.js
+++ app/js/main.js
@@ -1,38 +1,41 @@
+
+
 $(function () {
 
-  var mixer = mixitup('.our-work__items');
-
   $('.questions__list-item--active').find('.questions__list-info').slideDown(); //сделать при прокрутке
-
+  
   $('.navbar__btn').on('click', function () {
-
+    
     $(this).toggleClass('navbar__btn--active');
     $('.navbar-menu').toggleClass('navbar-menu--active');
-
+    
   })
-
+  
   $('.view-progects__item').hover(function () {
-
+    
     let popupIndex = $('.view-progects__popup')[$(this).index()];
     $('.view-progects__popup').removeClass('view-progects__popup--active');
     $(popupIndex).toggleClass('view-progects__popup--active');
-
+    
   })
 
   $('.testimonials__slider').slick({
     infinite: false,
     speed: 700,
   })
-
+  
   $('.questions__list-item').on('click', function () {
     $(this).toggleClass('questions__list-item--active');
-
-    if($(this).hasClass('questions__list-item--active')){
+    
+    if ($(this).hasClass('questions__list-item--active')) {
       $(this).find('.questions__list-info').slideDown(500);
-    }else{
+    } else {
       $(this).find('.questions__list-info').slideUp(500);
     }
-
+    
   })
 
+  var mixer = mixitup('.our-work__items');
+  
 });
+
