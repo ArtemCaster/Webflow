1ed4d7ec7e144cc34055c87476a34f4d79677b48
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2022-03-31 17:53:50 +0300

    fixed 0.1

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,7 @@ $(function () {
     {
     boxClass:     'wow',      // default
     animateClass: 'animate__animated', // default
-    offset:       300,          // default
+    offset:       100,          // default
     mobile:       false,
   }
   )
@@ -16,35 +16,37 @@ $(function () {
     scrollTracking();
   });
   
-  $('.questions__list-item--active').find('.questions__list-info').slideDown();
-
-
+  
+  
 
   function mix() {
     var mixer = mixitup('.our-work__items');
   }
-
-
-
+  
+  
+  
   $('.navbar__btn').on('click', function () {
 
     $(this).toggleClass('navbar__btn--active');
     $('.navbar-menu').toggleClass('navbar-menu--active');
-
+    
   })
-
+  
   $('.view-progects__item').hover(function () {
-
+    
     let popupIndex = $('.view-progects__popup')[$(this).index()];
     $('.view-progects__popup').removeClass('view-progects__popup--active');
     $(popupIndex).toggleClass('view-progects__popup--active');
-
+    
   })
-
+  
   $('.testimonials__slider').slick({
     infinite: false,
     speed: 700,
   })
+  
+
+  $('.questions__list-item--active').find('.questions__list-info').slideDown();
 
   $('.questions__list-item').on('click', function () {
     $(this).toggleClass('questions__list-item--active');
@@ -54,7 +56,6 @@ $(function () {
     } else {
       $(this).find('.questions__list-info').slideUp(500);
     }
-
   })
 
   mix();
